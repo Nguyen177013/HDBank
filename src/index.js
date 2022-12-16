ef6adae1bae94a750914a46b78e0dcf86727e90e
KhaiVox <khaiduchoa@gmail.com>
2022-12-16 15:16:23 +0700

    Phan Nguyen xem

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ app.use(cookieParser())
 
 // Body parse
 var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // JWT
 var jwt = require('jsonwebtoken')
@@ -112,61 +112,32 @@ app.get('/deleteCookie', function(req, res, next) {
     res.redirect('/')
 })
 
-// translate
-
-app.post('/translate', function(req, res) {
-    const axios = require('axios')
-    const encodedParams = new URLSearchParams()
-        // encodedParams.append('q', 'Hello, world!')
-        // encodedParams.append('target', 'vi')
-        // encodedParams.append('source', 'en')
-
-    encodedParams.append('q', 'Xin chào')
-    encodedParams.append('target', 'en')
-    encodedParams.append('source', 'vi')
-
-    const options = {
-        method: 'POST',
-        url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Accept-Encoding': 'application/gzip',
-            'X-RapidAPI-Key': '4c48a9c605msh9bf84f399eaca8fp195f31jsn8d75354b2a7f',
-            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-        },
-        data: encodedParams,
-    }
-
-    axios
-        .request(options)
-        .then(function(response) {
-            res.json(response.data)
-        })
-        .catch(function(error) {
-            console.error(error)
-        })
-})
-
-// SMS
-// app.post('/sendsms', function(req, res) {
+// Translate
+// app.post('/translate', function(req, res) {
 //     const axios = require('axios')
-
 //     const encodedParams = new URLSearchParams()
-//     encodedParams.append('sms', '+84971521473')
-//     encodedParams.append('message', 'Test message here.')
-//     encodedParams.append('senderid', 'MyCompany')
-//     encodedParams.append('schedule', '1377959755')
-//     encodedParams.append('return', 'http://yourwebsite.com')
-//     encodedParams.append('key', '1B490066-EA03-E39A-A18C-C4868E45CFAE')
-//     encodedParams.append('username', 'temp-idk-test-dynamic')
+
+//     // encodedParams.append('q', 'Hello, world!')
+//     // encodedParams.append('target', 'vi')
+//     // encodedParams.append('source', 'en')
+
+//     encodedParams.append('q', 'Tài khoản')
+//     encodedParams.append('q', 'Chuyển tiền')
+//     encodedParams.append('q', 'Nạp tiền điện thoại')
+//     encodedParams.append('q', 'Đổi mật khẩu')
+//     encodedParams.append('q', 'Đăng xuất')
+
+//     encodedParams.append('target', 'en')
+//     encodedParams.append('source', 'vi')
 
 //     const options = {
 //         method: 'POST',
-//         url: 'https://inteltech.p.rapidapi.com/send.php',
+//         url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
 //         headers: {
 //             'content-type': 'application/x-www-form-urlencoded',
+//             'Accept-Encoding': 'application/gzip',
 //             'X-RapidAPI-Key': '4c48a9c605msh9bf84f399eaca8fp195f31jsn8d75354b2a7f',
-//             'X-RapidAPI-Host': 'inteltech.p.rapidapi.com',
+//             'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
 //         },
 //         data: encodedParams,
 //     }
@@ -174,65 +145,43 @@ app.post('/translate', function(req, res) {
 //     axios
 //         .request(options)
 //         .then(function(response) {
-//             console.log(response.data)
+//             // res.json(response.data.data.translations[0].translatedText)
+//             res.json(response.data)
 //         })
 //         .catch(function(error) {
 //             console.error(error)
 //         })
 // })
 
-// const { Vonage } = require('@vonage/server-sdk')
+// SMS
+const accountSid = 'ACff0c2237d5222f6fcd7a328f3a61d70f'
+const authToken = '860513d9a9b6c02951b8596c35178516'
+const client = require('twilio')(accountSid, authToken)
 
-// const Vonage = require('@vonage/server-sdk')
-// const { NCCOBuilder, Talk, OutboundCallWithNCCO } = require('@vonage/voice')
+app.post('/sendsms', function(req, res) {
+    // xóa token "otp"
+    // let cookie = req.cookies
+    // for (var prop in cookie) {
+    //     if (!cookie.hasOwnProperty(prop)) {
+    //         continue
+    //     }
+    //     res.cookie(prop, '', { expires: new Date(0) })
+    // }
 
-// const vonage = new Vonage({
-//     apiKey: '24f3cd57',
-//     apiSecret: 'ijTDmo3XDKJi5HIt',
-//     applicationId: 'fd374c1a-9aeb-4ac5-b2d2-c947e33d01bc',
-//     privateKey: `-----BEGIN PRIVATE KEY-----
-//     MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCWqp6aOKhfbRsj
-//     a5SuuVai/TZuklqEvGxuwN4gpPoG7e2GJpXv61MbltGdcm9pO+D+5mW6g2J6jRT0
-//     qew1LKdPOVoRFA0uK+/Z6/uoDg6TQBp2/x25sAWe6LVtOs+tGEaGJLXW4etaCCG9
-//     smBVGTyoxEK+lFIugWgIz3KmecUCR9Lt72rF4JlqBE1fzUHh6l1edF0zfIac871Y
-//     QbjgKZJG7U4bm0P/HBE2YhCYoYVLUrIlJZPkDj7m3EVgDFGGiDtPVcNCNchAUS33
-//     KMerIfmkLm91r1qyPz90KlPNDE7Wygcr+VoPEGatiREbU5CloZTnJZ36j4VpXHT+
-//     Zh/3kwYjAgMBAAECggEAFMYSrIifrS9MfMjtDQhF9J7S01JqQ6n9OL9uq8lDe2gb
-//     22bU6T8k9tqygocllbi+sDamk8hgxYKlVIoHT6+dTqma4bGbkvWrklGyIaef2oK4
-//     rhaai8tELcxqk38hOlMXX5COjUHEzbrtorZbskVoB+GuTZECzHcQQpgOd/tghAya
-//     j06NeJ0xiz/MUzrnNbKzUib0WwwG0FP9sknsMSUDNHXVH0qVR8HzkNVhlkT8bxn3
-//     HVU+70XbE9g6ce9gVIXLN4Owl5GKivTrcsP2FtzZ4vyfXatetx+Yj12GIGfLxRpg
-//     Oln9p/pcj8PFsEf8Je6Md270JbLhjtLK4HVqKm8t2QKBgQDRJHPtSOp9N0XICM4b
-//     8dId5NsyJ27AhKEWra0uNEzD8ywuobg5rUS4HfGfZWHvIppQxb7pmkpHq2CIxgPW
-//     9Ix/8Q6HV01/NhY2/pgbnoebIWoHmqK68DFBIdcJA5jobmlLS0Nz7AV7prEZowtc
-//     62iYXOUSlw9YU3W4CfuR/TZNKQKBgQC4bDqiMfTHYzGrIJhv9BKvZ2b6831i2jHV
-//     hZ1o4pw+qw8MdA3tE9yIZkUEsv0tuHaNhazGnQ3PGKzeMUMezjrD8syhYFaw87HE
-//     XgUBnz+jWqHGlIccJMmUTNYXCwdjrnCldQUN8AJhR6BRODxd2RLifGCKpwkJ5xDN
-//     qaXzbe1WawKBgQC4ObUIQp+TlxmX3/G2eegNPye6YTPUz7+aW3heZ+FrBgxYM0o1
-//     PCR5Tp8lOkfvPvrRxa6q1acyxIpvUIXXVmlg1fVnUIbKVNlyrgdM5PMsul1Mv3Ey
-//     3Bt29RkXLp0FuSR1F6IePbyCtE3yeEWtHwxukf2Sx4/TzQi4CcsZSQQJKQKBgCeF
-//     4kbyR9AXPQS8lo6fkwWONm6fkgaGVlm5KdPZhLyVckMr9/QP5ppAuJfaOBH9T0Lj
-//     G/TwoY35bsqragQHFGb7+Mi42btQbo9zysTfOQcsHDFvkUkBxUWH9fW4QlwlfTSo
-//     UG39OrVYYU4mrCE8rSRmnVX5mCFMKLi6m7sAmvWDAoGAKWXrBrmU2OpVLuirCvyD
-//     dLUMfJPRNJyAGmEuww63yEBirRnLZDEXmXEH6DbVcM4Cwsb0Pi8a2ym3XIL7UqOH
-//     Qd9/lJN2AEt0Wht5EkG9hNIaR/g9WpG28C1oPL0Dy4dkHMR2urd8QeWbIqSE3Rd9
-//     3qcAv13aqLfuFhHFWzI+cDs=
-//     -----END PRIVATE KEY-----
-//     `,
-// })
+    // res.redirect('/')
 
-// app.post('/sendmess', function(req, res) {
-//     vonage.messages
-//         .send(
-//             new Text(
-//                 'This is a Facebook Messenger text message sent using the Messages API',
-//                 FB_RECIPIENT_ID,
-//                 FB_SENDER_ID,
-//             ),
-//         )
-//         .then((resp) => console.log(resp.message_uuid))
-//         .catch((err) => console.error(err))
-// })
+    var valueRandom = req.body
+    console.log(valueRandom)
+
+    client.messages
+        .create({
+            body: 'Mã OTP của bạn là: ' + req.cookies.otp,
+            messagingServiceSid: 'MG1fed4473d00d018f043553bd295d9338',
+            to: '+84971521473',
+        })
+        .then((message) => console.log(message.sid))
+        .done()
+})
 
 // Nhận các route sau đó sử dụng (luôn để dưới cùng)
 const route = require('./routes')
--- src/public/js/forgotPass.js
+++ src/public/js/forgotPass.js
@@ -1,23 +1,29 @@
+// Set Cookie
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date()
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+    let expires = 'expires=' + d.toUTCString()
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+}
+
+// Get Cookie
+function getCookie(cname) {
+    let name = cname + '='
+    let decodedCookie = decodeURIComponent(document.cookie)
+    let ca = decodedCookie.split(';')
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length)
+        }
+    }
+    return ''
+}
+
 // Quên mật khẩu
-
-// Refresh Token
-// async function refreshToken() {
-//     let data = await $.ajax({
-//         url: 'https://hdbank-hackathon.auth.ap-southeast-1.amazoncognito.com/oauth2/token',
-//         method: 'POST',
-//         headers: {
-//             accept: 'application/json',
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//         data: {
-//             client_id: 'sikcnei4t2h3ntkqj5d49ltvr',
-//             grant_type: 'refresh_token',
-//             refresh_token: 'eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.lMUJrc55tc2qdYBrU7any-E4sFKY0H21ViFgldna42qiaK44o2zEuIj648XcGGKP3uEfCunTj-asHm4zmsJnz6BS3fEuGJhzQlB_8BuP5lVs36g0OaQWsxtSiUoxby04GBIIJGVHP2w2NYzMd8unuqFEO_4K-cyqZlTb451-xNE9FvO-IgHDFsMPQdlkqvlPCIc3bjsDPlvCDtu5B0anO4o7V7A-vaGQ_YJHBBBvOVsD-o0Wl7EhTCu3urstn4w0PvFILhAn13zpH-Wp6jcQNP04K9hsrRL0apKWqG-_yvPSCxaoIWuzHiIWT7YfTKd92U5IcYu_GSyfXSV06pl7Nw.ZZWAjXdDaBa8-GLt.3U1Y-_QgsmFHC9nqiK1RrOFSYDb1J-5rFCJz8NhBQ7zUe_BpSKUVvrbqJNMFbKW9OVG541VFueuXR9faJVRcQtttrR2yoPOE5snXw7IjdsyDj45BUMXASRDMUjj95lKTMDQUkK5aJthB1PkV9dr6-yoHUxCD8PnVSBQnmAuD0pW7uRZ1ppeo1qbldzN-_DxGAtrXxbqsH7yb7zWfEZTStQaMq9o2j_dAMdk0xMkRVLKaJ1ERNBsgirMxnwp8FNL-j9f8U0UeKgdxzGNsTEuUMAPhBMPnAEeGmXhgvofW55xWM2-8mElbd7_xc6GnoWp3LNdo1rvJwlV5Iatmh4T1G2OWP63-0bvhL0HhZJNrrceCvDov5imD7pedP-JR_aw-Veyv4sR3UJ44EK80aoq4GGdxh5aLmAoqgKop3fDSa-olarZV9C8IhnswHa_wLQo8rUlS1cpqjK-RWF1h6HfPM79_qWF0rKuBVmKIiAU89ncRDxzusuNpH1oXs26bSa9t7MKKqVpGqvg1n8oTLn8g773dF_uZbFsiCCNAzrab5_veYs9lfyFS3HAlokRCkvuCKRkGQpvwes1se3fxy7jRxXXGK9mzwqFnMD-lhPTPXjQAvOUUKS-g9QugqHZTDqLH_YxfNcRSNTyuFqBN-qmsI2QGMXcdAMGUuU5GM6p55jfDqoeVaC7Nd0QMsGtKkl--MoIwSX-y_Nh6poq9Zll59SVzwfG24Yu2y5aCz_meFYifEfezYC2E2qDenlqh2XyzHOgc6PyJ1Pze6NOglHIWB34vdUfHMDb_wdWVapkVUmSVAsn98azqtQwKLYmTkVm86-PMfwwnNo4vZyXiZ6g_NUY1VDF5hQiVAQ5okJMZLeAOpEY2cBZMtbExp_84aznobV1CJEVHN_PXCAihYXsUfj-2MVqvbtB2aCKR-SDSiqTXF68UC4WGithQYU1q4tO2YEvVdTUsCUsRVB8ygvjaCVpF7xw3o-ZOJELTcIqxsHONi03f8dI84A8xLvrm8UB7zsPIJhpafRURanYbLiC07gz8Gs9s4LmUAWNUNFEubsQCLpfUPimzGVxotGRX4ijJbQEwf0gLdtOl1OdJLvMkG0a-qgjUkDw-2v-SEAK9WD7TgW1dMwT1rCm6kQkZSWI9Dq23Uqol8y6fov1Jxo6mo_RLfruLsUaMXivpoSuNKMWtlEe0-bk3f5KFvSKr6ElAaVwMt8oyRJlsjms2c0LpA_t2_ODJRwYYLbLvqILHw0XlpHja5oohBLJLh6xjnLda9BM2xExgqqO3QVKENdCErNjHqMSECNunLRNnt_ICG5RwcMlXhWq1mzJHfhf1M2ikvIsRCL-hdA.LT6g9msdRd_SszieL_X2Ng',
-//         },
-//     })
-//     return data.id_token
-// }
-
 const otpContainer = document.querySelector('.otp-container')
 const mobileVerify = document.querySelector('.mobile-verify')
 const btnContinue = document.querySelector('.btn-continue')
@@ -43,7 +49,27 @@ btnContinue.addEventListener('click', () => {
             random += Math.floor(Math.random() * 10).toString()
         })
         valueRandom = random
-        btnSendOtp.setAttribute('href', 'tel:' + valueRandom)
+
+        // fetch('/sendsms', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ valueRandom }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // }).then((data) => {
+        //     console.log('oke')
+        // })
+
+        $.ajax({
+            url: '/sendsms',
+            method: 'POST',
+            body: JSON.stringify({
+                valueRandom: valueRandom,
+            }),
+        }).then((data) => {
+            setCookie('otp', valueRandom, 1)
+            console.log(valueRandom)
+        })
+
+        //btnSendOtp.setAttribute('href', 'tel:' + valueRandom)
     }
 })
 
@@ -54,31 +80,24 @@ btnBack.addEventListener('click', () => {
     mobileVerify.classList.add('go-right', 'active-box')
 })
 
-btnVerify.addEventListener('click', () => {
-    let newPass = document.querySelector('#newPass').value.trim()
-    let repeatNewPass = document.querySelector('#repeatNewPass').value.trim()
-    let codeOtp = document.querySelector('#code-otp').value.trim()
+// btnVerify.addEventListener('click', () => {
+let formSubmit = document.querySelector('.form-action-forgot-password')
+formSubmit.addEventListener('submit', (e) => {
+        let newPass = document.querySelector('#newPass').value.trim()
+        let repeatNewPass = document.querySelector('#repeatNewPass').value.trim()
+        let codeOtp = document.querySelector('#code-otp').value.trim()
+        e.preventDefault()
 
-    if (newPass == '' || repeatNewPass == '' || codeOtp == '') {
-        // fix lỗi gửi sự kiện gửi đi khi chưa nhập thông tin tại đây --------------------------------------------------
-        alert('Vui lòng nhập đầy đủ thông tin !')
-    } else {
-        if (newPass !== repeatNewPass || valueRandom !== codeOtp) {
-            let formSubmit = document.querySelector('.form-action-forgott-password')
-            formSubmit.addEventListener('submit', (e) => {
-                e.preventDefault()
-            })
-            alert('Mật khẩu chưa trùng khớp !')
-                // fix lỗi gửi sự kiện gửi đi khi chưa nhập thông tin tại đây --------------------------------------------------
-                //handleForgotPass()
+        if (newPass == '' || repeatNewPass == '' || codeOtp == '') {
+            alert('Vui lòng nhập đầy đủ thông tin !')
+            return
+        } else if (newPass !== repeatNewPass) {
+            alert('Mật khẩu xác nhận chưa trùng khớp !')
+            return
+        } else if (valueRandom !== codeOtp) {
+            alert('Mã xác thực chưa chính xác !')
+            return
         } else {
-            let formSubmit = document.querySelector('.form-action-forgot-password')
-            formSubmit.addEventListener('submit', (e) => {
-                e.preventDefault()
-            })
-
-            // gọi ra tất cả thông tin có trong DB user
-            // so sánh để lấy ra username đúng như người dùng nhập ngoài input
             let apiKeyXata = 'xau_NFju5b34TYFnTVeex0PP51gJ1xGDoFmG0'
             $.ajax({
                 url: 'https://6151_v-kh-i-s-workspace-uh7b6d.us-east-1.xata.sh/db/HDBank:main/tables/users/query',
@@ -171,5 +190,5 @@ btnVerify.addEventListener('click', () => {
                 })
             })
         }
-    }
-})
+    })
+    // })
